6a041f4e947cc000d0da89b21ad2c975f4f0547b
mltplxr <128910989+mltplxr@users.noreply.github.com>
2023-11-06 14:23:33 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let id = Math.floor(Math.random() * 1000000);
+let img;
+
+function preload() {
+  img  = loadImage("holo.png");
+
+}
+
+function setup() {
+   createCanvas(300, 500);
+  background(255, 50);
+  // Display ID on canvas
+  textSize(12);
+  text(id, 10, 20);
+  image(img, 0, 0, 300, 300);
+  strokeWeight(0.1);
+  noFill();
+  
+}
+
+function draw() {
+
+
+for (let i = 0; i < height; i += 1) {// manipulate topography
+
+beginShape();
+
+for (let j = 0; j < width; j += 1.8) {// manipulate topography
+let noiseVal = noise(j/200, i/40); // 100, 100 > for smoothness 
+
+vertex(j, i + noiseVal * 100); // 200 < for smoothness
+
+}
+    endShape();
+  }
+
+  image(img, 100, 100, 100, 100);
+}
+// save png
+  function keyTyped() {
+  if (key === 's') {
+    saveCanvas('###', 'png');
+  }
+  }
